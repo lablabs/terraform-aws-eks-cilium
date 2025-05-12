99983679638580f6911850bd2c5919ac152b25ef
Attila Sovak <attila.sovak@gmail.com>
2025-05-12 11:06:20 +0200

    feat(cilium): add more moved to migrations

--- migrations.tf
+++ migrations.tf
@@ -9,7 +9,28 @@ moved {
   to   = module.addon.helm_release.this[0]
 
 }
+
 moved {
   from = helm_release.argo_application[0]
   to   = module.addon.helm_release.argo_application[0]
+}
+
+moved {
+  from = kubernetes_service_account.helm_argo_application_wait[0]
+  to   = module.addon.kubernetes_service_account.helm_argo_application_wait[0]
+}
+
+moved {
+  from = kubernetes_role_binding.helm_argo_application_wait[0]
+  to   = module.addon.kubernetes_role_binding.helm_argo_application_wait[0]
+}
+
+moved {
+  from = kubernetes_role.helm_argo_application_wait[0]
+  to   = module.addon.kubernetes_role.helm_argo_application_wait[0]
+}
+
+moved {
+  from = kubernetes_job.helm_argo_application_wait[0]
+  to   = module.addon.kubernetes_job.helm_argo_application_wait[0]
 }
